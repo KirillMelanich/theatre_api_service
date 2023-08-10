bfe3b50bcc91b249635d35485f89c047f56e1d76
KirillMelanich <kirillmelanich@gmail.com>
2023-08-10 16:43:55 +0300

    Create user models

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "user",
+    "theatre",
 ]
 
 MIDDLEWARE = [
@@ -98,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 
 # Internationalization
